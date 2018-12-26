0710c3b4bb2d49b9e71d18993a68f03ecf3fbdbb
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-26 00:55:08 -0200

    Backup do banco de dados da data

--- src/Model/DB/Backups/backup-2018-12-26.sql
+++ src/Model/DB/Backups/backup-2018-12-26.sql
@@ -327,6 +327,152 @@ LOCK TABLES `wallets` WRITE;
 INSERT INTO `wallets` VALUES (1,3,0.00,NULL,NULL,'2018-12-25 03:33:40'),(2,4,0.00,NULL,0,'2018-12-25 03:46:56');
 /*!40000 ALTER TABLE `wallets` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'db_library'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `create_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `create_book`(
+IN pname varchar(100),
+IN ptitle varchar(100),
+IN pdescription text,
+IN pid_publisher int,
+IN pid_author int,
+IN pcust_value float(6,2),
+IN prent_value float(6,2),
+IN psale_cust float(6,2),
+IN pammount int,
+IN pdate_create date,
+IN pstatus_book int
+)
+BEGIN
+	
+	INSERT INTO books (name, title, description, id_publisher, id_author, cust_value, rent_value, ammount, date_create, status_book, sale_value) VALUES (pname, ptitle, pdescription, pid_publisher, pid_author, pcust_value, prent_value, pammount, pdate_create, pstatus_book, psale_cust);
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `create_client` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `create_client`(
+IN pname varchar(100),
+IN pemail varchar(100),
+IN ppassword varchar(128),
+IN pphoto varchar(100),
+IN pstatus_client int
+)
+BEGIN
+	INSERT INTO clients (name, email, password, photo, status_client) VALUES (pname, pemail, ppassword, pphoto, pstatus_client);
+	
+    SET @id_client := (SELECT id_client FROM clients WHERE email = pemail);
+    INSERT INTO adresses (id_client) VALUES (@id_client);
+    INSERT INTO wallets (id_client) VALUES (@id_client);
+    
+    
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `delete_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `delete_book`(
+IN pid_book int
+)
+BEGIN
+	UPDATE books SET status_book = 0 
+    WHERE id_book = pid_book;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `delete_client` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `delete_client`(
+IN pid_client int
+)
+BEGIN
+	UPDATE clients SET status_client = 0 WHERE id_client = pid_client;
+    UPDATE wallets SET status_wallet= 0 WHERE id_client = pid_client;
+    UPDATE adresses SET status_address= 0 WHERE id_client = pid_client;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `update_book` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`gabriel`@`localhost` PROCEDURE `update_book`(
+IN pid_book int,
+IN pname varchar(100),
+IN ptitle varchar(100),
+IN pdescription text,
+IN pid_publisher int,
+IN pid_author int,
+IN pcust_value float(6,2),
+IN prent_value float(6,2),
+IN psale_value float(6,2),
+IN pammount int,
+IN pdate_create date,
+IN pstatus_book int
+)
+BEGIN
+	UPDATE books SET name = pname, title = ptitle, description = pdescription, id_publisher = pid_publisher, id_author = pid_author, cust_value = pcust_value, rent_value = prent_value, sale_value= psale_value, ammount = pammount, date_create = pdate_create, status_book = pstatus_book
+    WHERE id_book = pid_book;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -337,4 +483,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-12-26  0:51:57
+-- Dump completed on 2018-12-26  0:55:03
