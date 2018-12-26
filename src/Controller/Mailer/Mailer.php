cf4a28106e817168134402b99f59478be57f18da
Gabriel Oliveira <gabrieloliveradesouza9@gmail.com>
2018-12-26 00:32:23 -0200

    Alteraçoes na classe Mailer

--- src/Controller/Mailer/Mailer.php
+++ src/Controller/Mailer/Mailer.php
@@ -6,15 +6,18 @@ use App\Config;
 use PHPMailer\PHPMailer\PHPMailer;
 use Rain\Tpl;
 
-class Mailer
-{
-    private $mail;
+class Mailer {
+	
+	const USERNAME = "cursophp7hcode@gmail.com";
+	const PASSWORD = "<?password?>";
+	const NAME_FROM = "Hcode Store";
+	private $mail;
 	public function __construct($toAddress, $toName, $subject, $tplName, $data = array())
 	{
-        $app = new Config();
+		$env = new Config();
 		$config = array(
-			"tpl_dir"       => $app->getSite('directory').'/src/Views/email/',
-			"cache_dir"     => $app->getSite('directory')."/src/Views/cache/",
+			"tpl_dir"       => $env->getSite('directory')."/src/Views/email/",
+			"cache_dir"     => $env->getSite('directory')."/src/Views/cache/",
 			"debug"         => false
 	    );
 		Tpl::configure( $config );
@@ -34,22 +37,22 @@ class Mailer
 		//Ask for HTML-friendly debug output
 		$this->mail->Debugoutput = 'html';
 		//Set the hostname of the mail server
-		$this->mail->Host = $app->getEmail('host');
+		$this->mail->Host = 'smtp.gmail.com';
 		// use
 		// $this->mail->Host = gethostbyname('smtp.gmail.com');
 		// if your network does not support SMTP over IPv6
 		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-		$this->mail->Port = $app->getEmail('port');
+		$this->mail->Port = 587;
 		//Set the encryption system to use - ssl (deprecated) or tls
 		$this->mail->SMTPSecure = 'tls';
 		//Whether to use SMTP authentication
 		$this->mail->SMTPAuth = true;
 		//Username to use for SMTP authentication - use full email address for gmail
-		$this->mail->Username = $app->getEmail('email');
+		$this->mail->Username = $env->getEmail('username');
 		//Password to use for SMTP authentication
-		$this->mail->Password = $app->getEmail('password');
+		$this->mail->Password = $env->getEmail('password');
 		//Set who the message is to be sent from
-		$this->mail->setFrom($app->getEmail('email'), $app->getEmail('name'));
+		$this->mail->setFrom($env->getEmail('username'), $env->getEmail('name'));
 		//Set an alternative reply-to address
 		//$this->mail->addReplyTo('replyto@example.com', 'First Last');
 		//Set who the message is to be sent to
